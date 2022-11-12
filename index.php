c401625cd36d5c3508eed74bf7826374c3eca72b
Lara Simmer <67665135+larasimmer@users.noreply.github.com>
2022-11-12 16:03:14 -0300

    Text translation

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 for($i=0; $i<10; $i++) {
-    echo "Meu primeiro código em PHP! <strong>Hello world</strong><br/>";
+    echo "Meu primeiro código em PHP! <strong>Olá mundo!</strong><br/>";
 }
 
 ?>
