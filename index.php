37c57af905c7de608a9037b902620e0ec390106d
Lara Simmer <67665135+larasimmer@users.noreply.github.com>
2022-11-12 16:05:47 -0300

    Forcing file conflict

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 for($i=0; $i<10; $i++) {
-    echo "Meu primeiro código em PHP! <strong>Olá mundo!</strong><br/>";
+    echo "Meu primeiro código em PHP! <strong>Olá mundo123!</strong><br/>";
+    echo "Olá HCode"
 }
 
 ?>
